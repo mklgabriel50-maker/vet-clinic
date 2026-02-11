c3b47e73b71fb290cdf42d9897c2159fa6913311
mklgabriel50-maker <mklgabriel50@gmail.com>
2026-02-11 11:59:00 +0200

    Disable auth for testing

--- vet_frontend/src/App.jsx
+++ vet_frontend/src/App.jsx
@@ -14,7 +14,7 @@ export default function App() {
       <Navbar />
       <div className="max-w-6xl mx-auto p-4">
         <Routes>
-          <Route path="/login" element={<Login />} />
+          <Route path="/login" element={<Navigate to="/" replace />} />
 
           {/* NO AUTH (TEST MODE) */}
           <Route path="/" element={<Dashboard />} />
